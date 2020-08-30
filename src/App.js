a3c2e29cd685fddc31fca9328c4bd771d5875d39
Mario <Mario@Jadrankos-iMac.local>
2020-08-30 11:04:38 -0500

    added search page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React from "react";
 import "./App.css";
 import Home from "./pages/Home";
 import Header from "./components/header";
+import Footer from "./components/Footer";
+import SearchPage from "./pages/SearchPage";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
 	return (
 		//BEM
 
 		<div className="app">
-			<Header />
-			<Home />
-			{/* Cards */}
-			{/* Footer */}
-			{/* Search Page */}
-			{/*Header #2 */}
+			<Router>
+				<Header />
 
-			{/* ... */}
+				<Switch>
+					<Route path="/search">
+						<SearchPage />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+				<Footer />
+			</Router>
 		</div>
 	);
 }
