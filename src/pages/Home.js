3dd1a5b92bdafc9b47420e131af3338d8b350947
Mario <Mario@Jadrankos-iMac.local>
2020-08-29 19:08:05 -0500

    cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import Banner from "../components/banner";
+import Card from "../components/Card";
 // rfce
 // ES7 snippets
 
@@ -8,6 +9,40 @@ function Home() {
 	return (
 		<div className="home">
 			<Banner />
+			<div className="home__section">
+				<Card
+					src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im+w=720"
+					title="Unique Stays"
+					description="Unique activites we can do together, lead by a world of hosts,"
+				/>
+				<Card
+					src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im+w=720"
+					title="Unique Stays"
+					description="Unique activites we can do together, lead by a world of hosts,"
+				/>
+				<Card
+					src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im+w=720"
+					title="Unique Stays"
+					description="Unique activites we can do together, lead by a world of hosts,"
+				/>
+			</div>
+			<div className="home__section">
+				<Card
+					src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im+w=720"
+					title="Unique Stays"
+					description="Unique activites we can do together, lead by a world of hosts,"
+				/>
+				<Card
+					src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im+w=720"
+					title="Unique Stays"
+					description="Unique activites we can do together, lead by a world of hosts,"
+				/>
+				<Card
+					src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im+w=720"
+					title="Unique Stays"
+					description="Unique activites we can do together, lead by a world of hosts,"
+				/>
+			</div>
 		</div>
 	);
 }
