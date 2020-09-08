cf92040d79209e534d8dcf52c96931a97261f5a4
Mario <Mario@Jadrankos-iMac.local>
2020-09-07 21:39:33 -0500

    decompose folders

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyD0CJ4KAn_PJ8UDLyKuj4sBQkaJqgrGWXY",
+	authDomain: "airbnb-clone-43eb4.firebaseapp.com",
+	databaseURL: "https://airbnb-clone-43eb4.firebaseio.com",
+	projectId: "airbnb-clone-43eb4",
+	storageBucket: "airbnb-clone-43eb4.appspot.com",
+	messagingSenderId: "781941272126",
+	appId: "1:781941272126:web:acb5f5fc80f0a28f4aeb6f",
+	measurementId: "G-T4NRWNVZTN",
+};
